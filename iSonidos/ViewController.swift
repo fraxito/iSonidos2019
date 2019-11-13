8990f1e25d5534099cc1aad814a64a7035bccc0d
Cetys <cetys@cetyss-mac.local>
2019-11-13 13:40:16 +0100

    werrewwer

--- iSonidos/ViewController.swift
+++ iSonidos/ViewController.swift
@@ -7,11 +7,24 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var reproductor : AVAudioPlayer!
+    
+    @IBAction func sonidoPulsado(_ sender: UIButton) {
+        reproductor.play()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let rutaAlArchivo : URL = Bundle.main.url(forResource: "latigo", withExtension: "mp3")!
+        do {
+            try reproductor = AVAudioPlayer(contentsOf: rutaAlArchivo)
+        } catch {
+            
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
